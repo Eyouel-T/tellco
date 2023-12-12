312f1e74dd340e9f856440761f03531cd63b8af8
Eyouel <eeyoueltsegaye@gmail.com>
2023-12-13 01:34:38 +0300

    loader.py

---
+++ loader.py
@@ -0,0 +1,40 @@
+from doctest import debug_script
+from json import load
+from pydoc import describe
+from quopri import decodestring
+import pandas as pd
+import psycopg2
+
+def loadData():
+    conn = psycopg2.connect(
+        host="localhost",
+        port="5432",
+        database="tellco",
+        user="postgres",
+        password="root"
+    )
+    #cursor = conn.cursor()
+    cursor = conn.cursor('my_cursor_name', withhold=True)
+    cursor.execute("SELECT * FROM xdr_data limit 1000")
+        # Fetch a batch of rows
+    batch_size = 1000
+    rows = cursor.fetchmany(batch_size)
+    
+    data = [] 
+
+    while rows:
+        # Process the fetched data
+        data += rows
+        # Fetch the next batch
+        rows = cursor.fetchmany(batch_size)
+    columns = [desc[0] for desc in cursor.description]
+    cursor.close()
+    conn.close()  
+    df = pd.DataFrame(data, columns=columns) 
+    print(len(data))
+    
+    return (df)
+
+
+
+
